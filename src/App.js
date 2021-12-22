fecdedfabdb9d343fee9ab9c6ec8489ad64b06c8
Ebireni David <ebirenidavid@gmail.com>
2021-12-22 12:59:59 +0100

    redux cleanup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import { io } from "socket.io-client"
+
 
 function App() {
+  useEffect(() => {
+    const socket = io("http://localhost:3200")
+  }, [])
+
   return (
-    <div className="App">
-      
+    <div className="bg-green-300 h-screen w-screen">
+      sdfg
     </div>
   );
 }
