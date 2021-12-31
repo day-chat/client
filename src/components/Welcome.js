252c4ecb695b7112e13b0b19eeff0088b43c15f5
Ebireni David <ebirenidavid@gmail.com>
2021-12-31 13:39:17 +0100

    created the welcome page of the app

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { ReactComponent as Logo }  from '../welcomeChat.svg'
 
 function Welcome() {
     return (
         <div className='flex-grow'>
-            welc
+            <div className='flex flex-col items-center space-y-5 h-full justify-center'>
+                <h2 className='font-semibold text-3xl text-slate-700'>Welcome to Daychat, Dayveed</h2>
+                <Logo className='h-52' />
+                <p className='text-slate-700 font-light'>Send and recieve messages to friends and family on the app</p>
+            </div>
         </div>
     )
 }
